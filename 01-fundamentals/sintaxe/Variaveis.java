1105a0a818430a3526895368804085704d155623
Gabriel Carmo <gabriellcarmo.dev@gmail.com>
2026-08-14 17:30:18 -0300

    exercicios de variaveis e tipos primitivos

---
+++ 01-fundamentals/sintaxe/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Gabriel";
+        int idade = 25;
+        double altura = 1.88;
+        boolean estudando = true;
+        
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(estudando);
+        System.out.println("nome: meu nome é " + nome);
+    }
+    
+    
+}
